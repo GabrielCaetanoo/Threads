ae149701f2db86ab2d1b2d1e99368ab68461cf09
Gabriel Caetano <gabrielc0202@hotmail.com>
2024-05-14 22:56:22 -0300

    perfil

--- threads/src/Pages/profile/index.tsx
+++ threads/src/Pages/profile/index.tsx
@@ -1,7 +1,7 @@
-import { Container, Logo, Header, DivNome, DivFoto, Nome, NomePerfil, BotaoThreads, DivNomePerfil} from "./styled";
+import { Container, Logo, Header, DivNome, DivFoto, Nome, NomePerfil, BotaoThreads, DivNomePerfil, FotoPerfil, Biografia, TextoBiografia} from "./styled";
 
 import LogoSVG from "../../../src/assets/images/logo.svg";
-
+import Perfil from "../../../src/assets/images/Perfil.jpg";
 
 const Profile = () => {
 
@@ -13,17 +13,23 @@ const Profile = () => {
             <Header>
                 <DivNome>
                     <Nome>Gabriel Caetano </Nome> <br/>
-                    <DivNomePerfil>
-                    <NomePerfil>gabriel_caetanou</NomePerfil> 
-                    <BotaoThreads>threads.net</BotaoThreads> 
-                    </DivNomePerfil>
+                      <DivNomePerfil>
+                        <NomePerfil>gabriel_caetanou</NomePerfil> 
+                        <BotaoThreads>threads.net</BotaoThreads> 
+                      </DivNomePerfil>
 
                 </DivNome>
 
                 <DivFoto>
-                 dasdasdasdad
+                 <FotoPerfil src={Perfil} alt="teste"></FotoPerfil>
                 </DivFoto>
             </Header>
+
+            <Biografia>
+                <TextoBiografia>
+
+                </TextoBiografia>
+            </Biografia>
             
         </Container>
         
--- threads/src/Pages/profile/styled.ts
+++ threads/src/Pages/profile/styled.ts
@@ -13,7 +13,7 @@ export const GlobalStyles = createGlobalStyle`
 
 export const Container = styled.div`
     display: flex; 
-    width: 700px;
+    width: 610px;
     flex-direction: column;
     align-items: center;
     padding-top: 27px;
@@ -37,12 +37,14 @@ export const DivNome = styled.div`
     flex: 1;
     width: 100%;
     flex-direction: column;
-    justify-content: flex-start;
+    justify-content: flex-center;
 `;
 
 export const DivNomePerfil = styled.div`
     display: flex;
     justify-content: flex-start;
+    flex-direction: row;
+    margin-top: -10px;
 
 `;
 
@@ -50,32 +52,59 @@ export const DivFoto = styled.div`
     display: flex;
     flex: 1;
     width: 100%;
+    flex-direction: row;
+    justify-content: flex-end;
+    align-items: flex-start;
 `;
 
 export const Nome = styled.h1`
-    font-size: 20px;
+    font-size: 25px;
     color: #F3F5F7;
     font-family: "Roboto", sans-serif;
     font-weight: 500;
     text-decoration: none;
+    margin: 0;
+    border: 0;
+    margin-top: 5px;
 `;
 
 export const NomePerfil = styled.h2`
-    font-size: 13px;
+    font-size: 14px;
     color: #F3F5F7;
     font-family: "Roboto", sans-serif;
     font-weight: 300;
     text-decoration: none;
-    margin-top: -28px;
+    margin: 0;
+    border: 0;
 `;
 
 export const BotaoThreads = styled.button` 
-    padding: 10px;
+    padding: 5px 7px;
     border-radius: 10px;
     font-family: "Roboto", sans-serif;
     background-color: #3A3A3A;
     border: 0;
-
-
+    color: #616161;
+    font-size: 10px;
+    letter-spacing: 0.2px;
+    margin-left: 5px;
 `;
 
+export const FotoPerfil = styled.img`
+    border-radius: 50%;
+    width: 100%;
+    max-width: 85px;
+`;
+
+export const Biografia = styled.div`
+    display: flex;
+    flex-direction: column;
+`;
+
+export const TextoBiografia = styled.h3`
+    border: 0;
+    margin: 0;
+    font-family: "Roboto", sans-serif;
+    font-size: 15px;
+
+`;
